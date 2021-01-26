d6845e4c42b37affa5254c8bd54fa538a527dcdc
CSellers720 <charliejsellers@gmail.com>
2021-01-26 17:04:12 -0600

    Added webpack config for react/css

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'client', 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'client', 'index.js'),
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
