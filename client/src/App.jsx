424c22de852866e471e4d49a81494b15055a1a55
CSellers720 <charliejsellers@gmail.com>
2021-01-26 23:34:38 -0600

    Restructured main App component
    Moved state hooks around a bit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import Title from './Title.jsx';
 import Form from './Form.jsx';
-//import Display from './Display.jsx';
+import Display from './Display.jsx';
 
 const App = () => {
-  const [nameQuery, setNameQuery] = useState('');
   const [dptFilter, setDptFilter] = useState('');
   const [ageFilter, setAgeFilter] = useState('');
+  const [results, setResults] = useState([]);
+
 
   return (
-    <div className="appDiv">
-      <Title />
-      <Form setters={{ setNameQuery, setDptFilter, setAgeFilter }} />
-      {/* <Display /> */}
+    <div className="appWrapper">
+      <div className="appDiv">
+        <Title />
+        <Form
+          setDptFilter={setDptFilter}
+          setAgeFilter={setAgeFilter}
+          setResults={setResults}
+        />
+      </div>
+      <div className="displayWrapper">
+        <Display
+          results={results}
+          dptFilter={dptFilter}
+          ageFilter={ageFilter}
+        />
+      </div>
     </div>
   );
 };
