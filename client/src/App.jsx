2c1a348f7b3801a4b5f70252fe1b57ad64dba20a
CSellers720 <charliejsellers@gmail.com>
2021-01-26 18:03:50 -0600

    Created placeholder app to verify correct rendering

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [nameQuery, setNameQuery] = useState('');
+  const [dptFilter, setDptFilter] = useState('');
+  const [ageFilter, setAgeFilter] = useState('');
+
+  return (
+    <div className="appDiv">
+      TEST
+    </div>
+  );
+};
+
+export default App;
