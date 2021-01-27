b94f245225f10b36a93b803ee8843868afdcbbb4
CSellers720 <charliejsellers@gmail.com>
2021-01-26 18:59:53 -0600

    Imported styling for the app in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App.jsx';
+import globalStyle from '../style/global.css';
+import appStyle from '../style/App.css';
+import formStyle from '../style/Form.css';
 
 render(<App />, document.getElementById('app'));
