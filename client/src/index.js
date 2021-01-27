b74d3ab817efeac38df9e713c7e987464be2cb18
CSellers720 <charliejsellers@gmail.com>
2021-01-26 23:45:24 -0600

    Imported Display styling here

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,6 @@ import App from './App.jsx';
 import globalStyle from '../style/global.css';
 import appStyle from '../style/App.css';
 import formStyle from '../style/Form.css';
+import dispStyle from '../style/Display.css';
 
 render(<App />, document.getElementById('app'));
